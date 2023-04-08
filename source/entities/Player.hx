cb4743ee9f53cc88b87e8028b96e119742d1ea91
Toby Alden <toby.alden@gmail.com>
2023-04-08 12:14:30 -0700

    Add player sprite. Tweak physics

--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -11,42 +11,40 @@ import scenes.*;
 
 class Player extends Entity
 {
-    public static inline var MAX_RUN_SPEED = 100;
-    public static inline var MAX_AIR_SPEED = 150;
-    public static inline var RUN_ACCEL = 1300;
-    public static inline var AIR_ACCEL = 800;
-    public static inline var GRAVITY = 800;
-    public static inline var JUMP_POWER = 300;
-    public static inline var JUMP_CANCEL = 50;
-    public static inline var WALL_JUMP_POWER_X = MAX_AIR_SPEED;
-    public static inline var WALL_JUMP_POWER_Y = 275;
-    public static inline var MAX_FALL_SPEED = 400;
-    public static inline var MAX_FALL_SPEED_ON_WALL = 50;
+    public static inline var SPEED = 90;
+    public static inline var GRAVITY = 700;
+    public static inline var JUMP_POWER = 200;
+    public static inline var JUMP_CANCEL = 30;
+    public static inline var GRAVITY_MODIFIER_AT_PEAK = 0.5;
+    public static inline var MAX_FALL_SPEED = 300;
     public static inline var COYOTE_TIME = 1 / 60 * 5;
     public static inline var JUMP_BUFFER_TIME = 1 / 60 * 5;
-    public static inline var WALL_STICK_TIME = 0.2;
 
     private var sprite:Spritemap;
     private var velocity:Vector2;
     private var timeOffGround:Float;
     private var timeJumpHeld:Float;
-    private var timeWallStuck:Float;
 
     public function new(x:Float, y:Float) {
         super(x, y);
-        mask = new Hitbox(10, 10);
-        sprite = new Spritemap("graphics/player.png", 10, 10);
+		mask = new Hitbox(6, 12, -1, 0);
+        sprite = new Spritemap("graphics/player.png", 8, 12);
         sprite.add("idle", [0]);
+        sprite.add("run", [1, 2, 3, 2], 8);
+        sprite.add("jump", [4]);
+        sprite.add("wall", [5]);
+        sprite.add("skid", [6]);
         sprite.play("idle");
         graphic = sprite;
+        mask = new Hitbox(6, 12, 1, 0);
         velocity = new Vector2();
         timeOffGround = 0;
         timeJumpHeld = 0;
-        timeWallStuck = 0;
     }
 
     override public function update() {
         movement();
+        animation();
         super.update();
         if(Input.check("jump")) {
             timeJumpHeld += HXP.elapsed;
@@ -57,50 +55,22 @@ class Player extends Entity
     }
 
     private function movement() {
+        if(Input.check("left") && !isOnLeftWall()) {
+            velocity.x = -SPEED;
+        }
+        else if(Input.check("right") && !isOnRightWall()) {
+            velocity.x = SPEED;
+        }
+        else {
+            velocity.x = 0;
+        }
+
         if(isOnGround()) {
             timeOffGround = 0;
-            if(Input.check("left") && !isOnLeftWall()) {
-                velocity.x -= RUN_ACCEL * HXP.elapsed;
-            }
-            else if(Input.check("right") && !isOnRightWall()) {
-                velocity.x += RUN_ACCEL * HXP.elapsed;
-            }
-            else {
-                velocity.x = MathUtil.approach(
-                    velocity.x, 0, RUN_ACCEL * HXP.elapsed
-                );
-            }
         }
         else {
             timeOffGround += HXP.elapsed;
-            if(isOnWall()) {
-                if(
-                    isOnLeftWall() && Input.check("right")
-                    || isOnRightWall() && Input.check("left")
-                ) {
-                    timeWallStuck += HXP.elapsed;
-                }
-                else {
-                    timeWallStuck = 0;
-                }
-            }
-            if(!isOnWall() || timeWallStuck >= WALL_STICK_TIME) {
-                if(Input.check("left") && !isOnLeftWall()) {
-                    velocity.x -= AIR_ACCEL * HXP.elapsed;
-                }
-                else if(Input.check("right") && !isOnRightWall()) {
-                    velocity.x += AIR_ACCEL * HXP.elapsed;
-                }
-                else {
-                    velocity.x = MathUtil.approach(
-                        velocity.x, 0, AIR_ACCEL * HXP.elapsed
-                    );
-                }
-            }
         }
-
-        var maxSpeed = isOnGround() ? MAX_RUN_SPEED : MAX_AIR_SPEED;
-        velocity.x = MathUtil.clamp(velocity.x, -maxSpeed, maxSpeed);
 
         if(isOnGround()) {
             velocity.y = 0;
@@ -119,31 +89,14 @@ class Player extends Entity
                 velocity.y = -JUMP_POWER;
             }
         }
-        else if(isOnWall()) {
-            if(
-                Input.pressed("jump")
-                || Input.check("jump") && timeJumpHeld <= JUMP_BUFFER_TIME
-            ) {
-                velocity.y = -WALL_JUMP_POWER_Y;
-                if(isOnLeftWall()) {
-                    velocity.x = WALL_JUMP_POWER_X;
-                }
-                else {
-                    velocity.x = -WALL_JUMP_POWER_X;
-                }
-            }
-        }
 
         var gravity:Float = GRAVITY;
         if(Math.abs(velocity.y) < JUMP_CANCEL) {
-            gravity *= 0.5;
+            gravity *= GRAVITY_MODIFIER_AT_PEAK;
         }
         velocity.y += gravity * HXP.elapsed;
 
-        velocity.y = Math.min(
-            velocity.y,
-            isOnWall() ? MAX_FALL_SPEED_ON_WALL : MAX_FALL_SPEED
-        );
+        velocity.y = Math.min(velocity.y, MAX_FALL_SPEED);
 
         moveBy(
             velocity.x * HXP.elapsed,
@@ -177,6 +130,26 @@ class Player extends Entity
     private function isOnRightWall() {
         return collide("walls", x + 1, y) != null;
     }
+
+    private function animation() {
+        if(!isOnGround()) {
+            sprite.play("jump");
+            sprite.flipX = velocity.x < 0;
+        }
+        else if(velocity.x != 0) {
+            if(
+                velocity.x > 0 && Input.check("left")
+                || velocity.x < 0 && Input.check("right")
+            ) {
+                sprite.play("skid");
+            }
+            else {
+                sprite.play("run");
+            }
+            sprite.flipX = velocity.x < 0;
+        }
+        else {
+            sprite.play("idle");
+        }
+    }
 }
-
-
